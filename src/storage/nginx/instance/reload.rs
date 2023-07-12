892124c88b13667ec8989e1ab568d8f6d0ce612d
amigin <amigin@gmail.com>
2023-07-12 17:46:17 +0300

    Debugging

--- src/storage/nginx/instance/reload.rs
+++ src/storage/nginx/instance/reload.rs
@@ -1,6 +1,13 @@
 use tokio::process::Command;
 
 pub async fn reload() -> Result<String, String> {
+    let test_output = Command::new("nginx").arg("-t").output().await.unwrap();
+
+    println!("Nginx Test result: {:?}", test_output);
+    if !test_output.status.success() {
+        return Err(String::from_utf8_lossy(&test_output.stderr).to_string());
+    }
+
     let output = Command::new("nginx")
         .arg("-s")
         .arg("reload")
@@ -8,10 +15,10 @@ pub async fn reload() -> Result<String, String> {
         .await
         .unwrap();
 
-    println!("stderr: {:?}", output);
+    println!("Nginx Reload result: {:?}", output);
     if !output.status.success() {
         return Err(String::from_utf8_lossy(&output.stderr).to_string());
     }
 
-    return Ok(String::from_utf8_lossy(&output.stdout).to_string());
+    return Ok(String::from_utf8_lossy(&test_output.stdout).to_string());
 }
