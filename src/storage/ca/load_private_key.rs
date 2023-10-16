0b5f608459826779a1fe09080d525bb4f2d64349
amigin <amigin@gmail.com>
2023-10-16 18:07:15 +0300

    Fixes

--- src/storage/ca/load_private_key.rs
+++ src/storage/ca/load_private_key.rs
@@ -8,6 +8,8 @@ pub async fn load_private_key(app: &AppContext, ca_cn: &str) -> PemPrivateKey {
         .into_ca_data_path(ca_cn);
 
     let file_name = ca_path.into_private_key_file_name();
+
+    println!("Loading private key from file: {}", file_name);
     let content = tokio::fs::read(file_name.as_str()).await.unwrap();
     PemPrivateKey::from_bytes(content)
 }
